102b324e13605939e59dec495a5a44d0064d7f2d
irconde <ivanrodriguezconde@gmail.com>
2019-09-29 13:31:44 -0500

    Constrained-based group defined

--- app/src/main/java/com/ualr/constraintlayout/MainActivity.java
+++ app/src/main/java/com/ualr/constraintlayout/MainActivity.java
@@ -12,5 +12,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // TODO 03: We can manipulate the visibility of the view in a group by just modifying the visibility of the group
+        Group imageGroup = findViewById(R.id.image_group);
+        imageGroup.setVisibility(View.GONE);
     }
 }
